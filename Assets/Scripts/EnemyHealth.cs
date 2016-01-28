a21e698369fce54d2bb71737f897267e72381950
Tuomas <opinnaytepaahto@gmail.com>
2016-01-28 09:38:37 +0200

    NYT O SYNCATTU

---
+++ Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyHealth : MonoBehaviour {
+
+    public float maxHealth = 100f;
+    public float currentHealth = 0f;
+
+	// Use this for initialization
+	void Start () {
+        currentHealth = maxHealth;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
